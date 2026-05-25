d473c35962d99d7f934a19c5f6ce7625812d982a
GiggleShtter1 <carlospaling122@gmail.com>
2026-05-25 12:33:54 +0800

    Fixing

--- dist/_helpers/db.js
+++ dist/_helpers/db.js
@@ -3,8 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const config_json_1 = __importDefault(require("../config.json"));
-const promise_1 = __importDefault(require("mysql2/promise"));
 const sequelize_1 = require("sequelize");
 const account_model_1 = __importDefault(require("../accounts/account.model"));
 const refresh_token_model_1 = __importDefault(require("../accounts/refresh-token.model"));
@@ -12,18 +10,22 @@ const db = {};
 exports.default = db;
 initialize();
 async function initialize() {
-    const { host, port, user, password, database } = config_json_1.default.database;
-    const connection = await promise_1.default.createConnection({ host, port, user, password });
-    // Create DB if it doesn't exist
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-    // Connect to DB
-    const sequelize = new sequelize_1.Sequelize(database, user, password, { dialect: 'mysql' });
-    // Init models
+    const host = process.env.DB_HOST;
+    const port = parseInt(process.env.DB_PORT || '3306');
+    const user = process.env.DB_USER;
+    const password = process.env.DB_PASSWORD;
+    const database = process.env.DB_NAME;
+    // Removed these two lines:
+    // const connection = await mysql.createConnection({ host, port, user, password });
+    // await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    const sequelize = new sequelize_1.Sequelize(database, user, password, {
+        host,
+        port,
+        dialect: 'mysql',
+    });
     db.Account = (0, account_model_1.default)(sequelize);
     db.RefreshToken = (0, refresh_token_model_1.default)(sequelize);
-    // Define relationships
     db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
     db.RefreshToken.belongsTo(db.Account);
-    // Sync models with database
     await sequelize.sync();
 }
